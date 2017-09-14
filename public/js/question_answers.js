ba90e2136e33c19e53c247aa906738fe93222499
GeorgeSalari <x3m.giku@gmail.com>
2017-09-14 11:17:17 +0800

    fix ajax for answers on question_answer page

--- public/js/question_answers.js
+++ public/js/question_answers.js
@@ -2,11 +2,7 @@ var question_upvoted = $('#question_upvoted');
 var question_downvoted = $('#question_downvoted');
 var question_upvote_active = $('#question_upvote_active');
 var question_downvote_active = $('#question_downvote_active');
-
-var answer_upvoted = $('#answer_upvoted');
-var answer_downvoted = $('#answer_downvoted');
-var answer_upvote_active = $('#answer_upvote_active');
-var answer_downvote_active = $('#answer_downvote_active');
+var all_button = $('.answers');
 
 $('#edit_answer').click(function(){
   $('.edit_delete').each(function(index, item){
@@ -86,71 +82,70 @@ $(question_upvoted).submit(function(event){
   })
 }),
 
-$(answer_upvote_active).submit(function(event){
+$(all_button).submit(function(event){
   event.preventDefault();
-  var p_count = $('#answer_count_votes').text();
-  $.ajax({
-    url: '/answer_upvote',
-    method: 'post',
-    data: $(this).serialize(),
-    success: function(data) {
-      p_count = parseInt(p_count) + 1;
-      $('#answer_count_votes').text(p_count);
-      $(answer_upvote_active).addClass("hidden");
-      $(answer_downvote_active).addClass("hidden");
-      $(answer_upvoted).removeClass("hidden");
-    }
-  })
-}),
-
-
-$(answer_downvote_active).submit(function(event){
-  event.preventDefault();
-  var p_count = $(answer_count_votes).text();
-  $.ajax({
-    url:'/answer_downvote',
-    method: 'post',
-    data: $(this).serialize(),
-    success: function(data) {
-      p_count = parseInt(p_count) - 1;
-      $('#answer_count_votes').text(p_count);
-      $(answer_upvote_active).addClass("hidden");
-      $(answer_downvote_active).addClass("hidden");
-      $(answer_downvoted).removeClass("hidden");
-    }
-  })
-}),
-
-$(answer_downvoted).submit(function(event){
-  event.preventDefault();
-  var p_count = $(answer_count_votes).text();
-  $.ajax({
-    url:'/answer_downvote',
-    method: 'post',
-    data: $(this).serialize(),
-    success: function(data) {
-      p_count = parseInt(p_count) + 1;
-      $('#answer_count_votes').text(p_count);
-      $(answer_downvoted).addClass("hidden");
-      $(answer_upvote_active).removeClass("hidden");
-      $(answer_downvote_active).removeClass("hidden");
-    }
-  })
-}),
-
-$(answer_upvoted).submit(function(event){
-  event.preventDefault();
-  var p_count = $(answer_count_votes).text();
-  $.ajax({
-    url:'/answer_upvote',
-    method: 'post',
-    data: $(this).serialize(),
-    success: function(data) {
-      p_count = parseInt(p_count) - 1;
-      $('#answer_count_votes').text(p_count);
-      $(answer_upvoted).addClass("hidden");
-      $(answer_upvote_active).removeClass("hidden");
-      $(answer_downvote_active).removeClass("hidden");
-    }
-  })
+  var answer_id = $(this).data('id');
+  var presed_button = $(this).attr('id');
+  switch (presed_button) {
+    case 'answer_upvote_active_'+answer_id :
+    $.ajax({
+      url: '/answer_upvote',
+      method: 'post',
+      data: $(this).serialize(),
+      success: function(){
+        p_count = $("[data-id='" + answer_id + "']").last().text();
+        p_count = parseInt(p_count) + 1;
+        $("[data-id='" + answer_id + "']").last().text(p_count);
+        $("#answer_upvote_active_"+answer_id).addClass("hidden");
+        $("#answer_downvote_active_"+answer_id).addClass("hidden");
+        $("#answer_upvoted_"+answer_id).removeClass("hidden");
+      }
+    })
+    break;
+    case 'answer_downvote_active_'+answer_id :
+    $.ajax({
+      url:'/answer_downvote',
+      method: 'post',
+      data: $(this).serialize(),
+      success: function(){
+        p_count = $("[data-id='" + answer_id + "']").last().text();
+        p_count = parseInt(p_count) - 1;
+        $("[data-id='" + answer_id + "']").last().text(p_count);
+        $("#answer_downvote_active_"+answer_id).addClass("hidden");
+        $("#answer_upvote_active_"+answer_id).addClass("hidden");
+        $("#answer_downvoted_"+answer_id).removeClass("hidden");
+      }
+    })
+    break;
+    case 'answer_upvoted_'+answer_id :
+    $.ajax({
+      url: '/answer_upvote',
+      method: 'post',
+      data: $(this).serialize(),
+      success: function(){
+        p_count = $("[data-id='" + answer_id + "']").last().text();
+        p_count = parseInt(p_count) - 1;
+        $("[data-id='" + answer_id + "']").last().text(p_count);
+        $("#answer_upvoted_"+answer_id).addClass("hidden");
+        $("#answer_downvote_active_"+answer_id).removeClass("hidden");
+        $("#answer_upvote_active_"+answer_id).removeClass("hidden");
+      }
+    })
+    break;
+    case 'answer_downvoted_'+answer_id :
+    $.ajax({
+      url:'/answer_downvote',
+      method: 'post',
+      data: $(this).serialize(),
+      success: function(){
+        p_count = $("[data-id='" + answer_id + "']").last().text();
+        p_count = parseInt(p_count) + 1;
+        $("[data-id='" + answer_id + "']").last().text(p_count);
+        $("#answer_downvoted_"+answer_id).addClass("hidden");
+        $("#answer_downvote_active_"+answer_id).removeClass("hidden");
+        $("#answer_upvote_active_"+answer_id).removeClass("hidden");
+      }
+    })
+    break;
+  }
 })
